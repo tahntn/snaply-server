e05bd7a3efb10ee056b611e58ceb4cc2564daab5
quangvu0905 <superquang08@gmail.com>
2023-12-27 23:40:37 +0700

    fix: catch error user is exist in conversation

--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -6,6 +6,7 @@ import { TPayloadSendMessage } from '../types';
 export const sendMessageService = async (payload: TPayloadSendMessage) => {
   try {
     const { user, conversationsId, title } = payload;
+    const currentUserId = user?.id;
 
     //check conversation exist
     const conversation = await Conversation.findById(conversationsId);
@@ -13,24 +14,23 @@ export const sendMessageService = async (payload: TPayloadSendMessage) => {
       throw new ApiError(httpStatus.BAD_REQUEST, 'Conversation not found');
     }
 
-    //create new message
-    const newMessage = await Message.create({
-      title,
-      senderId: user?.id,
-      conversationsId,
+    // check user is in conversation
+    const existingConversation = await Conversation.find({
+      participants: {
+        $elemMatch: { $eq: currentUserId },
+      },
     });
 
-    //update conversation
-    await Conversation.findOneAndUpdate(
-      {
-        _id: conversationsId,
-      },
-      {
-        $push: {
-          messages: newMessage._id,
-        },
-      }
-    );
+    if (existingConversation.length <= 0) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'User is not in conversation');
+    }
+
+    // create new message
+    const newMessage = await Message.create({
+      title,
+      senderId: currentUserId,
+      conversationsId,
+    });
 
     return newMessage;
   } catch (error) {
