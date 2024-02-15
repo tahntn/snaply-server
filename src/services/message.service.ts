f5f23792fb424d1d099a052d3328144914c84e6d
tahntn <ntnhat.267@gmail.com>
2024-02-15 13:36:14 +0700

    fix: fix pusher message

--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -83,9 +83,11 @@ export const sendMessageService = async (payload: TPayloadSendMessage, pusher: P
       url,
     });
 
+    const _newMessage = await newMessage!.populate('replyTo');
     //trigger to conversation new message
     await pusher.trigger(conversationId.toString(), 'message:new', {
       ...newMessage.toObject(),
+      replyTo: _newMessage.replyTo,
       senderId: {
         username: user.username,
         email: user.email,
@@ -111,6 +113,9 @@ export const sendMessageService = async (payload: TPayloadSendMessage, pusher: P
 
       const _newConversationObj = {
         ..._newConversationPopulated.toObject(),
+        participants: _newConversationPopulated.participants.map(
+          (participants) => participants._id
+        ),
         lastActivity: {
           lastMessage: {
             ...newMessage.toObject(),
